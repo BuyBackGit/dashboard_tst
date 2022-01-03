54f0b662bb0de664dddafa2c3f2185d5c08f3b3b
BuyBackGit <info.buybacktoken@gmail.com>
2022-01-03 15:56:44 +0000

    EasySwap - factory - beta version

--- send.js
+++ send.js
@@ -1,5 +1,71 @@
 console.log('hello world');
 
+const abicreateaddress = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "date",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_defaultContract2",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "TokenAddress",
+				"type": "address"
+			}
+		],
+		"name": "Token_Created",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "children",
+		"outputs": [
+			{
+				"internalType": "contract GET_TOKEN",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "_defaultContract2",
+				"type": "address"
+			}
+		],
+		"name": "createSwap",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
+
 serverUrl = "https://pe6tgnkrykjf.usemoralis.com:2053/server";
 appId =  "OqhZPshkcGYtVulxkxcB7aomqgDqnEjlAbbkJZaN";
 Moralis.start({ serverUrl, appId});
@@ -212,6 +278,41 @@ getrefresh = async () => {
     window.location.reload();
 }
 
+getCreateButton = async () => {
+   
+    
+    console.log('Getting Address for Easy Swap Factory');
+    let addresstocreate = String(document.querySelector('#addressToCreate').value);      
+    alert('You will Create a Easy Swap for - ' + addresstocreate);
+    window.web3 = await Moralis.enableWeb3();
+    let contractInstance = new web3.eth.Contract(abicreateaddress, "0x7B16fC82041B5e9DF96149baFddd7ac579f1E788")
+    console.log(addresstocreate);
+   
+    const receipt = await contractInstance.methods.createSwap(addresstocreate).send({from: ethereum.selectedAddress});
+    console.log('processing');              
+
+    //This will print on console all the necessary data.
+
+    console.log(receipt.events);   
+           
+    //Get TokenAddress from console and create a pop up with the value for user copy 
+
+    
+    const contract_address = await receipt.events.Token_Created.returnValues['TokenAddress'];
+
+    console.log(contract_address);
+
+    console.log('Create Alert with Contract address');
+
+    alert('Please Copy the New Easy Swap Address Contract - ' + contract_address);
+
+    document.getElementById('neweasy-address').textContent = (contract_address);
+
+}
+
+  
+   
+
 
 
 
@@ -242,6 +343,9 @@ if(document.querySelector('#btn-get-native-balances') != null){
 } 
 if(document.querySelector('#btn-refresh') != null){
     document.querySelector('#btn-refresh').onclick = getrefresh;    
+}
+if(document.querySelector('#CreateButton') != null){
+    document.querySelector('#CreateButton').onclick = getCreateButton;    
 }
 
 
